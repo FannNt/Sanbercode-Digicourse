47c8fbb56ffeb62081bc1d80b30133b290eff50e
Fannn <affanhelmi111@gmail.com>
2025-02-25 22:29:32 +0700

    [+] Init

--- crud_project/routes/web.php
+++ crud_project/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CastController;
+use App\Http\Controllers\GameController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,4 +16,11 @@ Route::get('/cast/{cast_id}/edit', [CastController::class, 'edit']);
 Route::put('/cast/{cast_id}', [CastController::class, 'update'])->name('update');
 Route::delete('/cast/{cast_id}', [CastController::class, 'delete'])->name('delete');
 
+Route::post("/game/create", [GameController::class, 'create'])->name('game-create');
+Route::get("/game/create", [GameController::class, 'form'])->name('create-form');
+Route::get("/game/{id}", [GameController::class, 'read'])->name('game-show');
+Route::get("/game/", [GameController::class, 'index'])->name('game');
+Route::post("/game/update/{id}", [GameController::class, 'update'])->name('game-update');
+Route::get("/game/update/{id}", [GameController::class, 'formUpdate'])->name('game-update-form');
+Route::delete("/game/delete/{id}", [GameController::class, 'delete'])->name('game-delete');
 

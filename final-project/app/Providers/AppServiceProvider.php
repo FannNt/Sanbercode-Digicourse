f16df4c32d159725aac01850e34226b7554e655b
Fannn <affanhelmi111@gmail.com>
2025-03-02 04:12:30 +0700

    [-] remove user service controller and other

--- final-project/app/Providers/AppServiceProvider.php
+++ final-project/app/Providers/AppServiceProvider.php
@@ -8,9 +8,6 @@ use App\Interface\FilmRepositoryInterface;
 use App\Interface\GenreRepositoryInterface;
 use App\Service\FilmService;
 use App\Service\GenreService;
-use App\Service\UserService;
-use App\Http\Repository\UserRepository;
-use App\Interface\UserRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,9 +17,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
-        $this->app->bind(UserService::class, UserService::class);
-
         $this->app->bind(FilmRepositoryInterface::class, FilmRepository::class);
         $this->app->bind(FilmService::class,FilmService::class);
 
